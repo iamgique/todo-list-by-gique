7db569929f021f6748abf3d9b01629b15d403054
Gique <sakul.mon@ascendcorp.com>
2018-05-17 01:04:06 +0700

    add webhook

---
+++ src/main/java/com.gique.todo/controllers/WebhookController.java
@@ -0,0 +1,19 @@
+package com.gique.todo.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("webhook")
+public class WebhookController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    @ResponseStatus( HttpStatus.OK )
+    public ResponseEntity<String> webhook() {
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+}
