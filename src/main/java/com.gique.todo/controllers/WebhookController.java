abd1781b9859bde93889047590ebd253b62539ad
Gique <sakul.mon@ascendcorp.com>
2018-05-17 02:02:32 +0700

    modify webhook

--- src/main/java/com.gique.todo/controllers/WebhookController.java
+++ src/main/java/com.gique.todo/controllers/WebhookController.java
@@ -7,15 +7,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 @RestController
 @RequestMapping("webhook")
 public class WebhookController {
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     @ResponseStatus( HttpStatus.OK )
-    public ResponseEntity<String> webhook(HttpServletRequest request) {
-        return new ResponseEntity<>("Test" + String.valueOf(request), HttpStatus.OK);
+    public ResponseEntity<String> webhook() {
+        return new ResponseEntity<>("", HttpStatus.OK);
     }
 }
