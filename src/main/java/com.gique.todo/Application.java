9fa953773ec9eb4f99575664fb84fe6728cccca4
Gique <sakul.mon@ascendcorp.com>
2018-05-17 01:34:14 +0700

    test webhook

--- src/main/java/com.gique.todo/Application.java
+++ src/main/java/com.gique.todo/Application.java
@@ -1,5 +1,11 @@
 package com.gique.todo;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +14,7 @@ import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@LineMessageHandler
 public class Application {
 
     public static void main(String[] args) {
@@ -18,5 +25,16 @@ public class Application {
     public void init() {
         //log.warn("Memory: Max[{}], Free[{}], Total[{}]", Runtime.getRuntime().maxMemory(),
                 //Runtime.getRuntime().freeMemory(), Runtime.getRuntime().totalMemory());
+    }
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
     }
 }
--- src/main/java/com.gique.todo/controllers/WebhookController.java
+++ src/main/java/com.gique.todo/controllers/WebhookController.java
@@ -2,6 +2,7 @@ package com.gique.todo.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,7 +14,7 @@ public class WebhookController {
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     @ResponseStatus( HttpStatus.OK )
-    public ResponseEntity<String> webhook() {
-        return new ResponseEntity<>("", HttpStatus.OK);
+    public ResponseEntity<String> webhook(@RequestHeader(value = "X-Line-Signature", required = false) String signature {
+        return new ResponseEntity<>("Test" + String.valueOf(signature), HttpStatus.OK);
     }
 }
