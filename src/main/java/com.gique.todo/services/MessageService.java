9ec199d8912deef063321d05ee7210496e389f04
Gique <sakul.mon@ascendcorp.com>
2018-05-17 23:31:35 +0700

    modify messageservice about create task

--- src/main/java/com.gique.todo/services/MessageService.java
+++ src/main/java/com.gique.todo/services/MessageService.java
@@ -1,6 +1,5 @@
 package com.gique.todo.services;
 
-import com.gique.todo.Application;
 import com.gique.todo.models.TodoTaskModel;
 import com.gique.todo.utils.Util;
 import com.linecorp.bot.model.event.MessageEvent;
@@ -9,23 +8,18 @@ import com.linecorp.bot.model.message.TextMessage;
 import lombok.NonNull;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Optional;
-import java.util.regex.Pattern;
 
 @Service
 public class MessageService {
-    private static final Logger log = LogManager.getLogger(Application.class);
+    private static final Logger log = LogManager.getLogger(MessageService.class);
 
     @Autowired
     private DataSource dataSource;
@@ -52,11 +46,11 @@ public class MessageService {
             Optional.ofNullable(String.valueOf(event.getMessage().getText())).orElseThrow(() -> new Exception());
             String msg = String.valueOf(event.getMessage().getText());
 
-            if (checkCreateTodoFormat(msg)) {
+            if (util.checkCreateTodoFormat(msg)) {
                 TodoTaskModel todoTaskModel = splitTodoTask(msg);
                 todoTaskModel.setLineId(String.valueOf(event.getSource().getUserId()));
                 saveTodoTask(todoTaskModel);
-                return new TextMessage("Todo List \n Your Task: "
+                return new TextMessage("Todo List: \n"
                     + todoTaskModel.getTask() + " : " + util.getDueDate(todoTaskModel.getDate(), todoTaskModel.getTime()));
             } else if (msg.equals("edit")) {
                 return new TextMessage("Edit todo list");
@@ -70,7 +64,6 @@ public class MessageService {
         }
     }
 
-
     public TodoTaskModel splitTodoTask(String msg){
         log.info("splitTodoTask: {}", msg);
         TodoTaskModel responseModel = new TodoTaskModel();
@@ -82,32 +75,4 @@ public class MessageService {
         }
         return responseModel;
     }
-
-    public boolean checkCreateTodoFormat(String msg) {
-        log.info("checkCreateTodoFormat: {}", msg);
-        String regex = "(.*)\\s:\\s(\\d{1,2}/\\d{1,2}/\\d{1,2}|today|tomorrow)(\\s:\\s\\d{1,2}:\\d{1,2}|$)";
-        //String regex = "(\\w.*\\s:\\s)[^:]\\w.*";
-        //String regex = "/.*\\s:\\s.*\\s/g";
-        return Pattern.matches(regex, msg);
-    }
-
-    /*public static void main(String args[]) throws SQLException, ParseException {
-        saveTodoTask();
-        *//*String a = "aaaa : 20/12/18";
-        String b = "aaaa : 20/12/18 : dssadkl";
-        String c = "aaaa : 20/12/18 : 22:22";
-        String d = "aaaaaa";
-        String e = "aaa@aaa.com";
-        String f = "aaaa : : 20/12/18 : 22:22";
-        String g = "aaaa vvvv : 20/12/18 : 22:22";
-
-        System.err.println(checkCreateTodoFormat(a));
-        System.err.println(checkCreateTodoFormat(b));
-        System.err.println(checkCreateTodoFormat(c));
-        System.err.println(checkCreateTodoFormat(d));
-        System.err.println(checkCreateTodoFormat(e));
-        System.err.println(checkCreateTodoFormat(f));
-        System.err.println(checkCreateTodoFormat(g));*//*
-    }*/
-
 }
--- src/main/java/com.gique.todo/utils/Util.java
+++ src/main/java/com.gique.todo/utils/Util.java
@@ -1,20 +1,35 @@
 package com.gique.todo.utils;
 
-import org.joda.time.DateTime;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
+import java.util.Locale;
+import java.util.regex.Pattern;
 
 @Service
 public class Util {
+    private static final Logger log = LogManager.getLogger(Util.class);
 
-    public String getDueDate(String reqDate, String reqTime) throws ParseException {
+    public static boolean checkCreateTodoFormat(String msg) {
+        log.info("checkCreateTodoFormat: {}", msg);
+        String regex = "(.*)\\s:\\s(\\d{1,2}/\\d{1,2}/\\d{1,2}|today|tomorrow)(\\s:\\s\\d{1,2}:\\d{1,2}|$)";
+        //String regex = "(\\w.*\\s:\\s)[^:]\\w.*";
+        //String regex = "/.*\\s:\\s.*\\s/g";
+        return Pattern.matches(regex, msg);
+    }
+
+    public static String getDueDate(String reqDate, String reqTime) throws ParseException {
+        log.info("getDueDate: reqDate: {} reqTime: {}", reqDate, reqTime);
         String createdAt = "";
         SimpleDateFormat sdf = new SimpleDateFormat("d/M/yy");
-        Date d = sdf.parse(this.convertTodayTomorrowDate(reqDate));
+        Date d = sdf.parse(convertTodayTomorrowDate(reqDate));
 
         if(reqTime == null || reqTime.equals("")){
             sdf.applyPattern("yyyy-MM-dd 12:00");
@@ -26,18 +41,46 @@ public class Util {
         return createdAt;
     }
 
-    public String convertTodayTomorrowDate(String param) {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+    public static String convertTodayTomorrowDate(String param) {
+        log.info("convertTodayTomorrowDate: reqDate: {}", param);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd", new Locale("en", "EN"));
         Date d = new Date();
+        LocalDateTime now;
+
         if(param.equals("today")){
-            return dateFormat.format(d);
+            now = LocalDateTime.now();
         } else if(param.equals("tomorrow")){
-            DateTime dtOrg = new DateTime(d);
-            DateTime dtPlusOne = dtOrg.plusDays(1);
-            return dateFormat.format(dtPlusOne);
+            now = LocalDateTime.from(d.toInstant().atZone(ZoneId.of("UTC+07"))).plusDays(1);
         } else {
             return param;
         }
+
+        Instant instant = now.atZone(ZoneId.of("UTC+07")).toInstant();
+        Date dateFromOld = Date.from(instant);
+        return dateFormat.format(dateFromOld);
     }
+
+    /*public static void main(String args[]){
+
+        System.err.println(convertTodayTomorrowDate("today"));
+        System.err.println(convertTodayTomorrowDate("tomorrow"));
+
+        *//*saveTodoTask();
+        *//**//*String a = "aaaa : 20/12/18";
+        String b = "aaaa : 20/12/18 : dssadkl";
+        String c = "aaaa : 20/12/18 : 22:22";
+        String d = "aaaaaa";
+        String e = "aaa@aaa.com";
+        String f = "aaaa : : 20/12/18 : 22:22";
+        String g = "aaaa vvvv : 20/12/18 : 22:22";
+
+        System.err.println(checkCreateTodoFormat(a));
+        System.err.println(checkCreateTodoFormat(b));
+        System.err.println(checkCreateTodoFormat(c));
+        System.err.println(checkCreateTodoFormat(d));
+        System.err.println(checkCreateTodoFormat(e));
+        System.err.println(checkCreateTodoFormat(f));
+        System.err.println(checkCreateTodoFormat(g));*//*
+    }*/
 
 }
